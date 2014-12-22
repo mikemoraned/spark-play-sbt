3690ae02e16eeaf2194741d52922a283437d36f8
Mike Moran <mike@houseofmoran.com>
2014-12-22 21:38:33 +0100

    show word counts, over 1 minute chunks, grouped by frequency

--- src/main/scala/com/houseofmoran/spark/play/twitter/TwitterStream.scala
+++ src/main/scala/com/houseofmoran/spark/play/twitter/TwitterStream.scala
@@ -12,7 +12,7 @@ import org.apache.spark.streaming.twitter._
 object TwitterStream {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("TwitterStream").setMaster("local[*]")
-    val ssc = new StreamingContext(conf, Seconds(10))
+    val ssc = new StreamingContext(conf, Seconds(1))
 
     val oauthProperties = new Properties()
     oauthProperties.load(new FileReader(args(0)))
@@ -24,9 +24,15 @@ object TwitterStream {
     val twitterStream = TwitterUtils.createStream(ssc, None)
 
     val wordStream = twitterStream.flatMap(status => status.getText().split(" ").map((_, 1)))
-    val wordCountStream = wordStream.reduceByKey(_ + _)
+    val wordCountStream = wordStream.reduceByKeyAndWindow((a: Int, b:Int) => a + b, Seconds(60), Seconds(60))
 
-    wordCountStream.print()
+    wordCountStream.foreachRDD( wordCountRDD => {
+        for(wordGroup <- wordCountRDD.groupBy{ case (word, count) => count }.sortBy(_._1)) {
+          val count = wordGroup._1
+          val words = wordGroup._2.map(_._1).mkString(", ")
+          System.out.println(s"$count: $words")
+        }
+    });
 
     ssc.start()
     ssc.awaitTermination()
