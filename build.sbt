44bb95788dd1abab41241edabf46b30cc9b53f30
Mike Moran <mike@houseofmoran.com>
2014-12-26 21:58:08 +0100

    save word counts as (start, windowSize, word, count) tuples in parquet files

--- build.sbt
+++ build.sbt
@@ -6,7 +6,9 @@ scalaVersion := "2.11.4"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.2.0",
+  "org.apache.spark" %% "spark-sql" % "1.2.0",
   "org.apache.spark" %% "spark-streaming" % "1.2.0",
   "org.apache.spark" %% "spark-streaming-twitter" % "1.2.0",
-  "org.mortbay.jetty" % "servlet-api" % "3.0.20100224")
+  "org.mortbay.jetty" % "servlet-api" % "3.0.20100224",
+  "com.github.nscala-time" %% "nscala-time" % "1.6.0")
     
--- src/main/scala/com/houseofmoran/spark/play/twitter/TwitterStream.scala
+++ src/main/scala/com/houseofmoran/spark/play/twitter/TwitterStream.scala
@@ -2,18 +2,26 @@ package com.houseofmoran.spark.play.twitter
 
 import java.io.FileReader
 import java.util.Properties
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+
 import scala.collection.JavaConversions._
 
 import org.apache.spark._
 import org.apache.spark.streaming._
 import StreamingContext._
 import org.apache.spark.streaming.twitter._
+import org.apache.spark.sql.SQLContext
+import com.github.nscala_time.time.Imports._
 
 object TwitterStream {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("TwitterStream").setMaster("local[*]")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(1))
     ssc.checkpoint("checkpoint")
+    val sqlSc = new SQLContext(sc)
+    import sqlSc.createSchemaRDD
 
     val oauthProperties = new Properties()
     oauthProperties.load(new FileReader(args(0)))
@@ -24,18 +32,17 @@ object TwitterStream {
 
     val twitterStream = TwitterUtils.createStream(ssc, None)
 
+    val windowLength = Seconds(60)
+
     val wordStream = twitterStream.flatMap(status => status.getText().split(" "))
-    val wordCountStream = wordStream.countByValueAndWindow(Seconds(60), Seconds(60))
+    val wordCountStream = wordStream.countByValueAndWindow(windowLength, windowLength)
 
     wordCountStream.foreachRDD( wordCountRDD => {
-        for(wordGroup <- wordCountRDD.groupBy{ case (word, count) => count }.sortBy(_._1, false)) {
-          val count = wordGroup._1
-          val words = wordGroup._2.map(_._1).mkString(", ")
-          System.out.println(s"$count: $words")
-        }
-    });
+      val now = DateTime.now.toDateTimeISO
 
-    wordCountStream.saveAsTextFiles("dump", "_words")
+      wordCountRDD.map( wordCount => (now.toInstant().millis, windowLength.milliseconds, wordCount._1, wordCount._2))
+                  .saveAsParquetFile(s"wordcounts/start${ISODateTimeFormat.dateTime().print(now)}_window${windowLength.milliseconds}ms.parquet")
+    });
 
     ssc.start()
     ssc.awaitTermination()
