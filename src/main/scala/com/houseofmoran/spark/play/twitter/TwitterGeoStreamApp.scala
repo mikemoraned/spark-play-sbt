80c28bb7c459265c004370633bc97a4d031f6ded
Mike Moran <mike@houseofmoran.com>
2014-12-30 19:08:35 +0100

    periodically dump all geo tweets to gzipped files

--- src/main/scala/com/houseofmoran/spark/play/twitter/TwitterGeoStreamApp.scala
+++ src/main/scala/com/houseofmoran/spark/play/twitter/TwitterGeoStreamApp.scala
@@ -1,6 +1,7 @@
 package com.houseofmoran.spark.play.twitter
 
 import com.github.nscala_time.time.Imports._
+import org.apache.hadoop.io.compress.GzipCodec
 import org.apache.spark._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.streaming._
@@ -11,7 +12,8 @@ object TwitterGeoStreamApp {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("TwitterGeoStream").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    implicit val ssc = new StreamingContext(sc, Seconds(60))
+    val windowLength = Seconds(60)
+    implicit val ssc = new StreamingContext(sc, windowLength)
     ssc.checkpoint("checkpoint")
     val sqlSc = new SQLContext(sc)
     import sqlSc.createSchemaRDD
@@ -22,6 +24,11 @@ object TwitterGeoStreamApp {
 
     stream.foreachRDD( rdd => {
       rdd.takeSample(true, 10).foreach(println)
+
+      val now = DateTime.now.toDateTimeISO
+
+      rdd.
+        saveAsTextFile(s"geotweets/start${ISODateTimeFormat.dateTime().print(now)}_window${windowLength.milliseconds}ms.txt.gz", classOf[GzipCodec])
     });
 
     ssc.start()
