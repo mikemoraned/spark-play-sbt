34ae1e493233c911bb744cf491232908cc01d980
Mike Moran <mike@houseofmoran.com>
2014-12-31 15:20:09 +0100

    extend TwitterVisApp to show d3 home page
    following examples in http://www.eclipse.org/jetty/documentation/current/embedding-jetty.html

--- src/main/scala/com/houseofmoran/spark/play/twitter/TwitterVisApp.scala
+++ src/main/scala/com/houseofmoran/spark/play/twitter/TwitterVisApp.scala
@@ -6,8 +6,8 @@ import com.github.nscala_time.time.Imports._
 import org.apache.spark._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.streaming._
-import org.eclipse.jetty.server.{Server, Request}
-import org.eclipse.jetty.server.handler.AbstractHandler
+import org.eclipse.jetty.server.{Handler, Server, Request}
+import org.eclipse.jetty.server.handler.{HandlerList, ResourceHandler, AbstractHandler}
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
@@ -25,7 +25,15 @@ class HelloWorld extends AbstractHandler {
 object TwitterVisApp {
   def main(args: Array[String]): Unit = {
     val server = new Server(8080)
-    server.setHandler(new HelloWorld())
+
+    val resources = new ResourceHandler()
+    resources.setWelcomeFiles(Array[String]("index.html"))
+    resources.setResourceBase("./src/main/resources")
+
+    val handlers = new HandlerList()
+    handlers.setHandlers(Array[Handler]( resources, new HelloWorld() ))
+
+    server.setHandler(handlers)
     server.start()
     server.join()
   }
